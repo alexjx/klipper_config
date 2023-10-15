a7b04a38423231bd30cda5aeccda8fe5f7b19fe6
Jia Xin <alexjx@gmail.com>
2023-10-15 20:08:00 +0800

    chore: tweak settings

--- docs/resonance.sh
+++ docs/resonance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-for f in /tmp/resonances*.csv; do
+for f in /tmp/resonances*.csv /tmp/calibration_data_*.csv; do
   ~/klipper/scripts/calibrate_shaper.py $f -o /tmp/shaper_calibrate_$(basename $f .csv | cut -b12-).png && \
   rm -fv $f
 done
